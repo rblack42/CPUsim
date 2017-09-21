891772a8fe0476e921f0d24249459f540fb7a260
Roie Black (Github rblack42) <rblack@austincc.edu>
2017-09-21 16:13:25 -0500

    fixed memory tests

--- tests/test_memory.cpp
+++ tests/test_memory.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include "Memory.h"
 
+// this aet of tests check the delay operations
 TEST_CASE( "test write with no delay works", "memory" ){
     Memory mem(5,0,0);
     mem.write(1,1,2);
@@ -18,18 +19,19 @@ TEST_CASE( "test write delay of 2 works", "memory" ) {
 
 TEST_CASE( "test read with no delay works", "memory" ){
     Memory mem(5,0,0);
-    mem.write(1,1,2);
+    mem.read(1,1);
     REQUIRE(mem.ready());
 }
 
 TEST_CASE( "test read with delay of 2 works", "memory" ) {
     Memory mem(5,2,2);
-    mem.write(1,1,2);
+    mem.read(1,1);
     REQUIRE(!mem.ready());
     REQUIRE(!mem.ready());
     REQUIRE(mem.ready());
 }
 
+// These tests check memory read and write
 TEST_CASE( "read 8 bits after write with no delay works", "memory" ) {
     Memory mem(5,0,0);
     mem.write(1,1,2);
@@ -65,6 +67,21 @@ TEST_CASE( "read 16 bitsafter write with delay of 2 works", "memory" ) {
     Memory mem(5,2,2);
     mem.write(1,2,478);
     REQUIRE(!mem.ready());
+    REQUIRE(!mem.ready());
+    REQUIRE(mem.ready());
+    mem.read(1,2);
+    REQUIRE(!mem.ready());
+    REQUIRE(!mem.ready());
+    REQUIRE(mem.ready());
+    REQUIRE(mem.read_valid() == 478);
+}
+
+// check that attempting to acces memory when busy does not alter active delay
+TEST_CASE( "check that write while busy is ignored", "memory" ) {
+    Memory mem(5,2,2);
+    mem.write(1,2,478);
+    REQUIRE(!mem.ready());
+    mem.write(1,2,466);     // should not affect operation
     REQUIRE(!mem.ready());
     REQUIRE(mem.ready());
     mem.read(1,2);
